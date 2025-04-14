71c349941ef2ddb87bc3b55249fc0d5b537a836a
AhrorSulaymonov <ahrorsulaymonov494@gmail.com>
2025-04-14 11:07:30 +0500

    hello

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import Main from "../app/Main"
+import React from "react";
+import Main from "../app/Main";
 
 function MainPage() {
-    return <Main />
+  return <Main />;
 }
 
-export default MainPage
+export default MainPage;
